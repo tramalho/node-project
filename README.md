966cedceea8b847e7a1e9a1204775484af0b822a
tramalho <thiagofacul@gmail.com>
2024-11-10 22:24:47 -0300

    second lesson

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 Free course: https://www.udemy.com/course/intro-to-node-js-express/
 
+## Run
+
+
 ## Exercises
-[First Lesson](https://github.com/tramalho/node-project/tree/first_lesson)
+- [First Lesson](https://github.com/tramalho/node-project/tree/first_lesson)
+- [Second Lesson](https://github.com/tramalho/node-project/tree/second_lesson)
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('node:path');
+const hoganMiddleware = require('hogan-middleware');
+
+// Set the directory where the template files are located
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine" , "mustache");
+app.engine("mustache", hoganMiddleware.__express);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home', null);
+});
+
+app.get('/json', (req, res) => {
+    const data = {
+        greetings: 'Hello!!!',
+    };
+
+    res.json(data);
 });
 
 app.listen(3000);
